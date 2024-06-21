26c696a3182668df8f4030699d059de5803a46ac
RodrigoLee56 <sousarodrigo56@outlook.com>
2024-06-21 14:17:51 -0400

    Project crud-spring created

---
+++ crud-spring/src/main/java/com/rodrigolee/crud_spring/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.rodrigolee.crud_spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hello")
+public class HelloController {
+
+	@GetMapping
+	public String hello() {
+		return "Bonsoir Rodrigo, bonne soarée!";
+	}
+
+}
